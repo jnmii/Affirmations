51987b2e7a5017c7db0cbc0bbcfbf664f1b55fea
jnmii <mbuyambajoseph@gmail.com>
2023-06-28 21:22:48 -0400

    completed app

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
@@ -23,6 +24,7 @@ class ItemAdapter(
     // Each data item is just an Affirmation object.
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     /**
@@ -41,7 +43,11 @@ class ItemAdapter(
      */
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
-        holder.textView.text = context.resources.getString(item.stringResourceId)
+            holder.apply {
+                textView.text = context.resources.getString(item.stringResourceId)
+                imageView.setImageResource(item.imageResourceId)
+            }
+
     }
 
     /**
